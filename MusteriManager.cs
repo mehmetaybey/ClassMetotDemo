3c7f357ccf0141c9b2e7443a694d59f9dd7f52e8
Mehmet AYBEY <mehmetaybey@aof.anadolu.edu.tr>
2021-01-19 22:07:38 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi : " + musteri.Adi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi : " + musteri.Adi);
+        }
+        
+        public  void FaizArttirildi(Musteri musteri)
+        {
+            Console.WriteLine("Faiz arttırıldı  : " + musteri.Meslek);
+        }
+
+        public void FaizDusuruldu(Musteri musteri)
+        {
+            Console.WriteLine("Faiz düşürüldü : " + musteri.Meslek);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Gokhan";
+            musteri1.Soyadi = "Yeşilyurt";
+            musteri1.DogumTarihi = "1989";
+            musteri1.Meslek = "Tornacı";
+            musteri1.Faiz = 1.40;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Mehmet";
+            musteri2.Soyadi = "Aybey";
+            musteri2.DogumTarihi = "1989";
+            musteri2.Meslek = "işsiz";
+            musteri2.Faiz = 3.20;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Aşkın";
+            musteri3.Soyadi = "Küçükdeniz";
+            musteri3.DogumTarihi = "1992";
+            musteri3.Meslek = "planlama";
+            musteri3.Faiz = 1.21;
+
+            Musteri[] musteriler = new Musteri[] {musteri1, musteri2,musteri3};
+            foreach (Musteri musteri in musteriler) 
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.DogumTarihi);
+                Console.WriteLine(musteri.Meslek);
+                Console.WriteLine(musteri.Faiz);
+                
+            }
+
+            Console.WriteLine("-----MusteriManager-----");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.FaizArttirildi(musteri2);
+            musteriManager.FaizDusuruldu(musteri3);
+            musteriManager.Sil(musteri2);
+
+        }
+    }
+}
